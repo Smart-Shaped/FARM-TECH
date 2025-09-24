bce3520f6d5bc79716d9e7a7e6f89b0e6d67bc2a
StefanoSmartShaped <stefano.dimeo@smartshaped.com>
2025-09-24 16:03:51 +0200

    add - fix

--- backend/apps/minio/urls.py
+++ backend/apps/minio/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, re_path
-from .views import MinIOTokenView, MinIOProxyView
+from .views import MinIOTokenView, MinIOProxyView, minio_webhook
 
 app_name = 'minio'
 
 urlpatterns = [
     path('token/', MinIOTokenView.as_view(), name='minio_token'),
     re_path(r'^proxy/(?P<path>.*)$', MinIOProxyView.as_view(), name='minio_proxy'),
-    path('webhook/', views.minio_webhook, name='minio_webhook'),
+    path('webhook/', minio_webhook, name='minio_webhook'),
 ]
