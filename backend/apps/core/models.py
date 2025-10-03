c845a8ccab42d3635616c9af63bed69e180d96aa
Andrea Lapolla <andrea.lapolla@smartshaped.com>
2025-10-03 13:01:22 +0200

    update core models

--- backend/apps/core/models.py
+++ backend/apps/core/models.py
@@ -4,6 +4,11 @@ from django.db import models
 class Experiment(models.Model):
     
     name = models.CharField(max_length=255, primary_key=True)
+    contributor = models.CharField(max_length=255, blank=True)
+    email = models.EmailField(blank=True, null=True)
+    type = models.CharField(max_length=100, blank=True)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -57,7 +62,7 @@ class ProcessedDataset(models.Model):
     
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    path = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
     experiment_id = models.ForeignKey(Experiment, on_delete=models.CASCADE)
     type = models.CharField(max_length=100)
     last_update = models.DateTimeField(auto_now=True)
