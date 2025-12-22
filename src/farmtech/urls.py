fe75a4f4a9ee87c79291f14469c4bb9bd47cff51
StefanoSmartShaped <stefano.dimeo@smartshaped.com>
2025-12-22 15:28:24 +0100

    swagger disable

--- src/farmtech/urls.py
+++ src/farmtech/urls.py
@@ -31,17 +31,17 @@ urlpatterns += [
     path("inference/", view=TemplateView.as_view(template_name="inference.html")),
     path("uploader/", view=dataset.uploader_view, name="uploader"),
     # ------------ Swagger/OpenAPI endpoints ------------
-    re_path(
-        r"^swagger(?P<format>\.json|\.yaml)$",
-        SchemaView.without_ui(cache_timeout=0),
-        name="schema-json",
-    ),
-    path(
-        "swagger/",
-        SchemaView.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    path("redoc/", SchemaView.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    # re_path(
+    #   r"^swagger(?P<format>\.json|\.yaml)$",
+    #   SchemaView.without_ui(cache_timeout=0),
+    #   name="schema-json",
+    # ),
+    # path(
+    #   "swagger/",
+    #   SchemaView.with_ui("swagger", cache_timeout=0),
+    #   name="schema-swagger-ui",
+    # ),
+    # path("redoc/", SchemaView.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     # ------------ Auth ------------
     path(
         "api/auth/keycloak/", auth.KeycloakAuthAPIView.as_view(), name="keycloak-auth"
